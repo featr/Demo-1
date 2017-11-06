536b0b3bea4d528440ada33394d491c7fc572dfb
Evlogi Aleksandrov <evlogi.aleksandrov@gmail.com>
2017-11-06 17:33:11 +0200

    ...

--- js/calculator.js
+++ js/calculator.js
@@ -6,6 +6,8 @@ const calculateBMI = e => {
 	let result = Math.round(bodyWeight / Math.pow(bodyHeight, 2) * 100) / 100;
 	if (typeof result === 'number' && !isNaN(result)) {
 		document.querySelector('#result').innerHTML = 'Your BMI is ' + result;
+	} else {
+		document.querySelector('#result').innerHTML = 'invalid input';
 	}
 };
 
